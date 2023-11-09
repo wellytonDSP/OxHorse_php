b7f7d6b9e22b732c785c1683a7aa8fa1d5ca543d
Wellyton dos Santos Pedroso <103326847+wellytonDSP@users.noreply.github.com>
2023-11-09 15:48:01 -0300

    atua

--- public/index.php
+++ public/index.php
@@ -3,17 +3,33 @@
 ?>
 <main>
     <section class="banners banner-1 d-flex flex-column justify-content-center text-center">
-        <div class="banners-titulo">
-            <div class="container form-contact">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <div class="banners-titulo row">
+            <div class="col-md-6">
+                <p>Tudo que você procura na pecuária em um só lugar</p>
+            </div>
+
+            <div class="container form-contact col-md-6">
+                <div class="title p-3">
+                    <h4 class="title">Juntece a nós para o futuro</h4>
+                </div>
+                <div class="mb-2">
+                    <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                        placeholder="Digite seu nome">
+                </div>
+                <div class="mb-2">
+                    <label for="exampleFormControlInput1" class="form-label">Email</label>
                     <input type="email" class="form-control" id="exampleFormControlInput1"
                         placeholder="Digite um email para contato">
                 </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div class="mb-2">
+                    <label for="exampleFormControlInput1" class="form-label">Telefone</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                        placeholder="Digite um email para contato com DDD">
                 </div>
+                <button class="btn-logar mt-3">
+                    Enviar
+                </button>
             </div>
         </div>
     </section>
