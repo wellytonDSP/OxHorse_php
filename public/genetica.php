bf83b03595b3d5836e9ced36f2c8edd0aad588a5
Wellyton dos Santos Pedroso <103326847+wellytonDSP@users.noreply.github.com>
2024-03-18 12:59:34 -0300

    genetica

--- public/genetica.php
+++ public/genetica.php
@@ -3,119 +3,78 @@ require_once("../templates/header.php");
 ?>
 <div class="album py-5 bg-body-tertiary">
     <div class="container">
-        <section>
-            <h3>Touro de Corte:</h3>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="../img/Fotos_Braford/Fotos_03.jpeg" class="card-img-top" alt="foto-angus">
-                        </svg>
-                        <div class="card-body">
-                            <div class="product-infos ">
-                                <div class="product-titles">
-                                    <h1 class="title-card">ARG-4414 / Marcos</h1>
-                                    <h2 class="subtitle-card">Proprietario: Fazenda Margorete</h2>
-                                </div>
-                                <div class="cotainer d-flex">
-                                    <div class="price">
-                                        R$319
-                                        
-                                    </div><div class="quantity">,00/10 doses</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="../img/Fotos_Angus/Foto_02.jpeg" class="card-img-top" alt="foto-angus">
-                        <div class="card-body">
-                            <div class="product-infos ">
-                                <div class="product-titles">
-                                    <div class="title">José</div>
-                                </div>
-                                <div class="infos ">
-                                    <div class="info overflow">
-                                        Pai:
-                                        <span class="info-value uppercase">PRUDENTE DA KAYLUA</span>
-                                    </div>
-                                    <div class="info overflow">
-                                        Nascimento:
-                                        <span class="info-value uppercase">19/09/2018</span>
-                                    </div>
-                                    <div class="info overflow">
-                                        Proprietário:
-                                        <span class="info-value uppercase">PDS AGROPECUARIA LTDA / WELLYTON DOS SANTOS
-                                            PEDROSO</span>
-                                    </div>
-                                </div>
+        <div class="row">
 
-                                <div class="cotainer">
-                                    <div class="price">
-                                        R$ 345,00
-                                        <span class="quantity">/10 doses</span>
-                                    </div>
-                                    <div class="d-flex align-items-start">
-                                        <div>
-                                            <button class="btn btn-success btn-cards">Detalhes</button>
-                                        </div>
-                                        <div>
-                                            <button class="btn btn-success btn-cards mr-auto">Adicionar ao
-                                                Carrinho</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="product-box card shadow-sm">
-                        <img src="../img/Fotos_Nelore/Foto_02.jpeg" class="card-img-top" alt="foto-angus">
-                        <div class="card-body">
-                            <div class="product-infos ">
-                                <div class="product-titles">
-                                    <div class="title">Amenadieu</div>
-                                </div>
-                                <div class="infos ">
-                                    <div class="info overflow">
-                                        Pai:
-                                        <span class="info-value uppercase">PRUDENTE DA KAYLUA</span>
-                                    </div>
-                                    <div class="info overflow">
-                                        Nascimento:
-                                        <span class="info-value uppercase">19/09/2018</span>
-                                    </div>
-                                    <div class="info overflow">
-                                        Proprietário:
-                                        <span class="info-value uppercase">TJG AGROPECUARIA LTDA / BRUNO GERALDO BORGES
-                                            MORANDO</span>
-                                    </div>
-                                </div>
-
-                                <div class="cotainer">
-                                    <div class="price">
-                                        R$ 285,00
-                                        <span class="quantity">/10 doses</span>
-                                    </div>
-                                    <div class="d-flex align-items-start">
-                                        <div>
-                                            <button class="btn btn-success btn-cards">Detalhes</button>
-                                        </div>
-                                        <div>
-                                            <button class="btn btn-success btn-cards mr-auto">Adicionar ao
-                                                Carrinho</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
+            <div class="col-md-3">
+                <h3>Filtro</h3>
             </div>
-        </section>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-3">
+
+                        <div class="card shadow-sm">
+                            <img src="../img/Fotos_Braford/Fotos_03.jpeg" class="card-img-top" alt="foto-angus">
+                            </svg>
+                            <div class="card-body">
+                                <div class="product-infos ">
+                                    <div class="product-titles">
+                                        <h1 class="title-card">ARG-4414 / Marcos</h1>
+                                        <h2 class="subtitle-card">Proprietario: Fazenda Margorete</h2>
+                                    </div>
+                                    <div class="cotainer d-flex">
+                                        <div class="price">
+                                            R$319
+
+                                        </div>
+                                        <div class="quantity">,00/10 doses</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card shadow-sm">
+                            <img src="../img/Fotos_Angus/Foto_02.jpeg" class="card-img-top" alt="foto-angus">
+                            <div class="card-body">
+                                <div class="product-infos ">
+                                    <div class="product-titles">
+                                        <h1 class="title-card">ARG-4414 / Marcos</h1>
+                                        <h2 class="subtitle-card">Proprietario: Fazenda Margorete</h2>
+                                    </div>
+                                    <div class="cotainer d-flex">
+                                        <div class="price">
+                                            R$319
+
+                                        </div>
+                                        <div class="quantity">,00/10 doses</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card shadow-sm">
+                            <img src="../img/Fotos_Nelore/Foto_02.jpeg" class="card-img-top" alt="foto-angus">
+                            <div class="card-body">
+                                <div class="product-infos ">
+                                    <div class="product-titles">
+                                        <h1 class="title-card">ARG-4414 / Marcos</h1>
+                                        <h2 class="subtitle-card">Proprietario: Fazenda Margorete</h2>
+                                    </div>
+                                    <div class="cotainer d-flex">
+                                        <div class="price">
+                                            R$319
+
+                                        </div>
+                                        <div class="quantity">,00/10 doses</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php
